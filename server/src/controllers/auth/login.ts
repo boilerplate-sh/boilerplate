81f6429349bb775fb7189df90ed39dd181510f95
Eli <69530035+elisvcodes@users.noreply.github.com>
2023-05-11 13:43:25 -0400

    slight updated to services

--- server/src/controllers/auth/login.ts
+++ server/src/controllers/auth/login.ts
@@ -1,5 +1,5 @@
 import { Request, Response } from "express";
-import { prismaClient } from "../../utils/prismaClient";
+import { prismaClient } from "../../services/prismaClient";
 import bcrypt from "bcrypt";
 import jwt from "jsonwebtoken";
 
--- server/src/controllers/auth/register.ts
+++ server/src/controllers/auth/register.ts
@@ -1,5 +1,5 @@
 import { Request, Response } from "express";
-import { prismaClient } from "../../utils/prismaClient";
+import { prismaClient } from "../../services/prismaClient";
 import bcrypt from "bcrypt";
 import { User } from "@prisma/client";
 import { assert, object, string, size, refine } from "superstruct";
