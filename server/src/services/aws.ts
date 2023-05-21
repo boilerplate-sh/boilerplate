278e099c2540dabe4d8cd986c0a38dab00fdb6ed
Eli <69530035+elisvcodes@users.noreply.github.com>
2023-05-21 14:33:03 -0400

    removed s3 for now

--- server/src/services/aws.ts
+++
@@ -1,9 +0,0 @@
-import aws from "aws-sdk";
-
-aws.config.update({
-  secretAccessKey: process.env.SECRET_ACCESS_KEY,
-  accessKeyId: process.env.ACCESS_KEY_ID,
-  region: process.env.REGION,
-});
-
-export const s3 = new aws.S3();
