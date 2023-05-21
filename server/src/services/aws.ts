3adad70772bbf1c0f7bb86f02867df0c82b64e2d
Eli <69530035+elisvcodes@users.noreply.github.com>
2023-05-21 14:25:59 -0400

    created an aws instance

---
+++ server/src/services/aws.ts
@@ -0,0 +1,9 @@
+import aws from "aws-sdk";
+
+aws.config.update({
+  secretAccessKey: process.env.SECRET_ACCESS_KEY,
+  accessKeyId: process.env.ACCESS_KEY_ID,
+  region: process.env.REGION,
+});
+
+export const s3 = new aws.S3();
